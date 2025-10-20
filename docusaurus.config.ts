f0ab3314ed0ec4f5bd63499a76752901242f96f1
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-10-20 14:15:46 +0300

    config update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Ahmet-MahirDEMIRELLI', // Usually your GitHub org/user name.
-  projectName: 'skysec-docs-docusaurus', // Usually your repo name.
+  projectName: 'Ahmet-MahirDEMIRELLI.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
